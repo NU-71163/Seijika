a8de22b6955ecd02aee8dfc2642398c7b4523b9e
NU-71163 <ciru22163@g.nihon-u.ac.jp>
2024-12-20 17:01:13 +0900

    編集

---
+++ Assets/Script/Quizs.cs
@@ -0,0 +1,185 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Linq;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+
+public class Quizs : MonoBehaviour
+{
+    private TextAsset csvFile;  // CSVファイル
+    private List<string[]> csvData = new List<string[]>();  // CSVファイルの中身を入れるリスト
+    int index; //問題番号
+    int correct; //正解数
+    bool AnswerFlag;    //解答判定
+    float timelimit = 5f; //制限時間
+    float nowtimer = 0f;   //経過時間
+    public Text ProblemLog;  //問題文
+    public Text ProblemNum;  //問題番号
+    public Text[] ChoiceLog;    //選択肢文字
+    public Text CorrectNum;   //正解数
+    public Text time;    //残り時間表示
+    public Button[] ChoiceBtns;   //選択肢
+    private List<int> numbers = new List<int> { 1, 2, 3, 4 };   //選択肢番号
+    private List<int> Problem = new List<int> { }; //未正解問題
+    public Slider TimeSlider;   //残り時間ゲージ
+
+
+    void Start()
+    {
+        //CSVロード
+        csvFile = Resources.Load("test") as TextAsset;
+        StringReader reader = new StringReader(csvFile.text);
+        while (reader.Peek() != -1)
+        {
+            string line = reader.ReadLine();
+            csvData.Add(line.Split(','));
+        }
+
+        //CSVの問題をリストにいれる
+        Problem = new List<int>(csvData.Count);
+        for (int i = 0; i < csvData.Count; i++)
+        {
+            Problem.Add(i);
+        }
+        Shuffle(Problem);  //問題をシャッフル
+        Debug.Log("Problem: " + string.Join(", ", Problem.Select(x => x.ToString())));
+
+
+        TimeSlider.value = 1f;  //制限時間ゲージ
+
+        // 最初の問題を表示
+        ShowNextQuestion();
+    }
+
+    void Update()
+    {
+        //時間制御
+        nowtimer += Time.deltaTime; // タイマー
+        float t = nowtimer / timelimit;// スライダーの値ー正規化
+        TimeSlider.value = Mathf.Lerp(1f, 0f, t);
+        float TimeLimit = 10f - nowtimer; //残り時間
+        TimeLimit = Mathf.Max(TimeLimit, 0f);
+
+        string LimitLog = TimeLimit.ToString("F0");
+        time.text = LimitLog + "秒";
+        time.color = (TimeLimit > 5.5f) ? Color.green : Color.red;// 5.5秒以上は緑、5.5秒未満は赤
+
+        // タイムオーバー
+        if (nowtimer >= timelimit)
+        {
+            nowtimer = 0f;     //タイマーを0秒に戻す
+            AnswerFlag = false;
+            StartCoroutine(NextQuestion());
+        }
+    }
+
+    IEnumerator NextQuestion()
+    {
+        yield return new WaitForSeconds(1.5f);  // 1.5秒待つ
+
+        ShowNextQuestion();
+    }
+
+    void ShowNextQuestion()
+    {
+        string No = (index + 1).ToString(); //問題番号
+        ProblemNum.text = "第" + No + "問";
+
+        // 問題と選択肢を表示
+        if (Problem.Count > index)
+        {
+            if (index < 10) //10問連続で出題、0から始めているから未満
+            {
+                Quiz();
+            }
+            else
+            {
+                Time.timeScale = 0;
+                Debug.Log("問題終了");
+            }
+        }
+        else if (Problem.Count == index)
+        {
+
+            Debug.Log("終了!!!!");
+
+        }
+    }
+    void Quiz()
+    {
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            int randomrow = Problem[index];
+            int randomcol = numbers[i];
+            ProblemLog.text = csvData[randomrow][0];
+            ChoiceLog[i].text = csvData[randomrow][randomcol];
+
+            // すでにイベントが追加されている場合は一旦解除してから追加
+            ChoiceBtns[i].onClick.RemoveAllListeners();
+            ChoiceBtns[i].onClick.AddListener(() => OnButtonClick(randomcol));
+        }
+
+        // タイマーをリセット
+        ResetTimer();
+        Shuffle(numbers);
+    }
+
+
+     void OnButtonClick(int selectedAnswer)
+        {
+         if (selectedAnswer == 1)
+         {
+             AnswerFlag = true;
+         }
+         else
+         {
+             AnswerFlag = false;
+         }
+
+         AnswerCheck();
+         // 次の問題を表示
+         StartCoroutine(NextQuestion());
+     }
+
+
+        void AnswerCheck()
+        {
+            ResetTimer();
+            if (AnswerFlag == true)//正解
+            {
+                correct += 1;   //正解数
+
+            }
+            index += 1;//次の問題へ
+
+            //正解の割合
+            float Rate = correct / (float)index * 100;
+            string Ratenum = Rate.ToString("F0");
+            string correctNum = correct.ToString();
+            CorrectNum.text = correctNum;
+        }
+
+        void ResetTimer()
+        {
+            nowtimer = 0f;
+            Time.timeScale = 1.0f;
+        }
+
+        // Fisher-Yatesシャッフル
+        void Shuffle<T>(List<T> list)
+        {
+            int n = list.Count;
+            for (int i = n - 1; i > 0; i--)
+            {
+                int j = Random.Range(0, i + 1);
+                T temp = list[i];
+                list[i] = list[j];
+                list[j] = temp;
+            }
+        }
+}
