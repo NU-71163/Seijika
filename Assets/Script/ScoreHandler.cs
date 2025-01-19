29f7b713c855fe163595d9ba3ce5aa1b43155c7a
NU-71163 <ciru22163@g.nihon-u.ac.jp>
2025-01-19 13:45:54 +0900

    編集

--- Assets/Script/ScoreHandler.cs
+++ Assets/Script/ScoreHandler.cs
@@ -18,17 +18,17 @@ public class ScoreHandler : MonoBehaviour
     public void EvaluateScore()
     {
         x = ResultDisplay.Goukei;
-        if (x >= 0 && x <= 40)
+        if (x >= 0 && x <= 40) 
         {
-            DisplayRandomMessage(new string[] { "すごい！", "頑張ったね！" });
+            DisplayRandomMessage(new string[] { "ソー・政治家の堕落により、日本の消費税が50%になった。", "金融政策の「ソーセージミクス」を提案して、実行直後は良い評価を得ていたが、裏金問題が発覚し議員を辞職した。" });
         }
         else if (x >= 41 && x <= 80)
         {
-            DisplayRandomMessage(new string[] { "まぁまぁだね", "普通だ" });
+            DisplayRandomMessage(new string[] { "ソー・政治家は頑張ったが、日本の消費税が15%になった。", "金融政策の「ソーセージミクス」を提案したが、日本の景気は悪くなってしまった。" });
         }
         else if (x >= 81 && x <= 100)
         {
-            DisplayRandomMessage(new string[] { "ひどいね", "最悪" });
+            DisplayRandomMessage(new string[] { "ソー・政治家の活躍により、日本の消費税が3%になった。", "金融政策の「ソーセージミクス」を提案したことにより、日本の景気が良くなった。" });
         }
     }
 
@@ -37,7 +37,7 @@ public class ScoreHandler : MonoBehaviour
         string message = messages[Random.Range(0, messages.Length)];
         resultText.text = message;
         Debug.Log($"表示されるメッセージ: {message}");
-        StartCoroutine(ChangeSceneAfterDelay(10f));
+        StartCoroutine(ChangeSceneAfterDelay(20f));
     }
 
     IEnumerator ChangeSceneAfterDelay(float delay)
